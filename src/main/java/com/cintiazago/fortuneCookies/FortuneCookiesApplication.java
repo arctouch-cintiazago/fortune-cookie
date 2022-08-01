80b165977f8baf9e2f8136057cc818c5beb61b27
Cintia Zago <cintiazago@Cintia-Zago-MacBook-Pro-579A.local>
2022-08-01 16:48:02 -0300

    Firt commit

---
+++ src/main/java/com/cintiazago/fortuneCookies/FortuneCookiesApplication.java
@@ -0,0 +1,13 @@
+package com.cintiazago.fortuneCookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneCookiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneCookiesApplication.class, args);
+	}
+
+}
